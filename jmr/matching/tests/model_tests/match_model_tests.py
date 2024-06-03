04c81821ea9e9ff006cc8843c7fb7c73f1e3f5b1
steven <sfriggs21@gmail.com>
2024-06-03 12:18:06 -0600

    added test_can_match_by_days_and_hours

--- jmr/matching/tests/model_tests/match_model_tests.py
+++ jmr/matching/tests/model_tests/match_model_tests.py
@@ -100,3 +100,25 @@ class MatchModelTests(TestCase):
         company1.job_requirements.save()
         matches = Match.find_exact(company=company1)
         self.assertEqual(matches, [user1])
+
+    def test_can_match_by_days_and_hours(self):
+        user1 = self.user1
+        user1.preferences.days_and_hours = {
+            'MON': ('1000', '1400'),
+            'WED': ('1000', '1400'),
+            'FRI': ('1000', '1400'),
+        }
+        user1.preferences.save()
+        user2 = self.user2
+        user2.preferences.remote = True
+        user2.preferences.hybrid = True
+        user2.preferences.save()
+        company1 = self.company1
+        company1.job_requirements.days_and_hours = {
+            'MON': ('1000', '1400'),
+            'WED': ('1000', '1400'),
+            'FRI': ('1000', '1400'),
+        }
+        company1.job_requirements.save()
+        matches = Match.find_exact(company=company1)
+        self.assertEqual(matches, [user1])
