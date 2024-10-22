f662a38737c2bbf75d1fa9ca7753b8681fa2c9e5
steven <sfriggs21@gmail.com>
2024-10-22 10:07:56 -0600

    updated jmr2 admin

--- jmr2/company/admin.py
+++ jmr2/company/admin.py
@@ -1,3 +1,47 @@
+# TODO: enhance admin site by using inlines
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+# class PositionInline(admin.StackedInline):
+#     model = Position
+#     extra = 1
+# 
+# class TaskInline(admin.TabularInline):
+#     model = Position.tasks.through
+#     extra = 1
+# 
+# class SkillInline(admin.TabularInline):
+#     model = Position.skills.through
+#     extra = 1
+# 
+class SkillDetailInline(admin.TabularInline):
+    model = SkillDetail
+    extra = 1
+
+class TraitDetailInline(admin.TabularInline):
+    model = TraitDetail
+    extra = 1
+
+# class CompanyAdmin(admin.ModelAdmin):
+#     inlines = [PositionInline]
+# 
+# class PositionAdmin(admin.ModelAdmin):
+#     inlines = [TaskInline, SkillDetailInline]
+# 
+class SkillAdmin(admin.ModelAdmin):
+    inlines = [SkillDetailInline]
+
+class TraitAdmin(admin.ModelAdmin):
+    inlines = [TraitDetailInline]
+
+
+# admin.site.register(Company, CompanyAdmin)
+admin.site.register(Company)
+# admin.site.register(Position, PositionAdmin)
+admin.site.register(Position)
+admin.site.register(Task)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Trait, TraitAdmin)
--- jmr2/company/models.py
+++ jmr2/company/models.py
@@ -4,15 +4,76 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField()
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'companies'
+
 class Position(models.Model):
     company = models.ForeignKey(Company, models.CASCADE)
 
     title = models.CharField()
     description = models.TextField()
 
-    required_skills = models.ManyToManyField('Skill', related_name='required_by')
-    preferred_skills = models.ManyToManyField('Skill', related_name='preferred_by')
-    bonus_skills = models.ManyToManyField('Skill', related_name='bonus_for')
+    tasks = models.ManyToManyField('Task')
 
+    skills = models.ManyToManyField('Skill', through='SkillDetail')
 
+    traits = models.ManyToManyField('Trait', through='TraitDetail')
 
+    def __str__(self):
+        return f'{self.title} ({self.company})'
+
+class Task(models.Model):
+    tag = models.CharField()
+
+    @property
+    def stub(self):
+        return '-'.join(self.tag.strip().lower().replace(r'[^a-z -]', '').split()[:5])
+
+    def __str__(self):
+        return self.tag
+
+class Skill(models.Model):
+    tag = models.CharField(max_length=25)
+
+    @property
+    def stub(self):
+        return self.tag.strip().lower().replace(r'\s+', '-')
+
+class SkillDetail(models.Model):
+    position = models.ForeignKey(Position, models.CASCADE)
+    skill = models.ForeignKey(Skill, models.CASCADE)
+
+    level = models.PositiveSmallIntegerField()
+    requirement_level = models.CharField(
+        choices={
+            'required': 'Required',
+            'preferred': 'Preferred',
+            'bonus': 'Bonus',
+        }
+    )
+
+class Trait(models.Model):
+    tag = models.CharField(max_length=25)
+
+    @property
+    def stub(self):
+        return self.tag.strip().lower().replace(r'\s+', '-')
+
+    def __str__(self):
+        return self.tag
+
+class TraitDetail(models.Model):
+    position = models.ForeignKey(Position, models.CASCADE)
+    trait = models.ForeignKey(Trait, models.CASCADE)
+
+    level = models.PositiveSmallIntegerField()
+    requirement_level = models.CharField(
+        choices={
+            'required': 'Required',
+            'preferred': 'Preferred',
+            'bonus': 'Bonus',
+        }
+    )
