40e7b0369e5ec34c869392cf71e5c88d93b93b0c
steven <sfriggs21@gmail.com>
2025-02-14 01:18:05 -0700

    begin change views to json

--- jmr2/backend/company/views.py
+++ jmr2/backend/company/views.py
@@ -6,18 +6,19 @@ from .models import *
 
 
 def benefits_index(req):
-    benefits = serializers.serialize('json', Benefit.objects.all())
+    benefits = json.dumps([{'pk': b.pk, 'tag': b.tag} for b in Benefit.objects.all()])
 
     return JsonResponse(benefits, safe=False)
 
 def benefit_page(req, pk):
-    benefit = serializers.serialize('json', Benefit.objects.filter(pk=pk))
+    benefit = Benefit.objects.get(pk=pk)
+    benefit_json = json.dumps({'pk': pk, 'tag': benefit.tag})
 
-    return JsonResponse(benefit, safe=False)
+    return JsonResponse(benefit_json, safe=False)
 
 
 def companies_index(req):
-    companies = serializers.serialize('json', Company.objects.all())
+    companies = json.dumps([{'pk': c.pk, 'name': c.name} for c in Company.objects.all()])
 
     return JsonResponse(companies, safe=False)
 
@@ -33,25 +34,27 @@ def company_page(req, pk):
 
 
 def positions_index(req):
-    positions = serializers.serialize('json', Position.objects.all())
+    positions = json.dumps([{'pk': p.pk, 'title': p.title} for p in Position.objects.all()]
 
     return JsonResponse(positions, safe=False)
 
 def position_page(req, pk):
-    position = serializers.serialize('json', Position.objects.filter(pk=pk))
+    position = Position.objects.get(pk=pk)
+                           position_json = json.dumps({'pk': pk, 'title': title, 'company': position.company.name, 'description': position.description, 'location': position.get_location(), 'relocation_assistance': position.get_relocation_assistance(), 'position_type': position.get_position_type(), 'pay': position.pay, 'pay_frequency': position.get_pay_frequency(), 'hours': position.get_hours(), 'benefits': [{'pk': b.pk, 'tag': b.tag, 'description': b.description} for b in position.benefits.all()], 'tasks': [{'pk': t.pk, 'tag': t.tag} for t in position.tasks.all()], 'skills': {'required': position.get_required_skills(), 'preferred': position.get_preferred_skills(), 'bonus': position.get_bonus_skills()}, 'traits': {'required': position.get_required_traits(), 'preferred': position.get_preferred_traits(), 'bonus': position.get_bonus_traits()})
 
-    return JsonResponse(position, safe=False)
+    return JsonResponse(position_json, safe=False)
 
 
 def skills_index(req):
-    skills = serializers.serialize('json', Skill.objects.all())
+                                                       skills = json.dumps([{'pk': s.pk, 'tag': s.tag} for s in Skill.objects.all()])
 
     return JsonResponse(skills, safe=False)
 
 def skill_page(req, pk):
-    skill = serializers.serialize('json', Skill.objects.filter(pk=pk))
+    skill = Skill.objects.get(pk=pk)
+                                                       skill_json = json.dumps({'pk': pk, 'tag': skill.tag})
 
-    return JsonResponse(skill, safe=False)
+    return JsonResponse(skill_json, safe=False)
 
 
 def tasks_index(req):
