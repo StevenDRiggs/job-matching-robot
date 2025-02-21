3e47c8019948dee9012b4d9d80ac4982b945d5a4
steven <sfriggs21@gmail.com>
2025-02-21 15:37:12 -0700

    added company_id to Company

--- jmr2/backend/company/models/Company.py
+++ jmr2/backend/company/models/Company.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+from uuid import uuid4
+
 from django.db import models
 
 
 class Company(models.Model):
     name = models.CharField()
+    company_id = models.UUIDField(default=uuid4 , editable=False)
 
     def __str__(self):
         return self.name
