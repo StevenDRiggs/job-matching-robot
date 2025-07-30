24fe13eaf78da6f9e6dd55cf1e3825a22d3cad10
steven <sfriggs21@gmail.com>
2025-07-29 22:29:10 -0600

    added init tests

---
+++ jmr3/backend/tests/test_init.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+
+from ..models import *
+
+
+class TestInit:
+    def test_create_engine(self):
+        assert create_engine('sqlite://', echo=True)
+
+    def test_create_metadata(self):
+        engine = create_engine('sqlite://', echo=True)
+        assert Base.metadata.create_all(engine) is None
